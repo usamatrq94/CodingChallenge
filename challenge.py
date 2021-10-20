cb48146ee0bdede8132609c94b9b991919f2e0fe
Moeen Riaz <moeen@Moeens-MacBook-Pro.local>
2021-10-20 13:59:33 +1100

    required data

--- challenge.py
+++ challenge.py
@@ -1,1 +1,20 @@
 import json
+
+filepath = input("Enter your JSON file path ")
+
+df_nested_list = pd.json_normalize(data, record_path =['dates'])
+
+df_required = pd.json_normalize(
+    data, 
+    record_path =['dates'], 
+      meta=[
+        ['sections', 'raceType'], 
+        ['sections', 'meetings', 'id'],
+          ['sections', 'meetings', 'name'],
+          ['sections', 'meetings', 'events','raceNumber'],
+          ['sections', 'meetings', 'events','httpLink'],
+          ['sections', 'meetings', 'events','id'],
+          ['sections', 'meetings', 'events','distance'],
+          ['sections', 'meetings', 'events','startTime'],
+    ],errors='ignore'
+)
