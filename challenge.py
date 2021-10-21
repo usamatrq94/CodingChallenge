f7e210424fe5740b1c513424f590c9436fb55a5a
Muhammad Tariq <63507947+usamatrq94@users.noreply.github.com>
2021-10-21 19:08:43 +1100

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -1,6 +1,7 @@
 #Importing important libraries
 import pandas as pd
 import json
+from datetime import datetime, timedelta
 
 #Asking user to input path to JSON file
 file_path = input("Enter your JSON file path ")
@@ -24,14 +25,11 @@ df = df_required[['dates.sections.meetings.regionName','dates.sections.meetings.
 
 #changing time zone to Australia/Brisbane and AEST iso formatting
 df['startTime'] = pd.to_datetime(df['startTime'], unit='s')
-df.set_index('startTime', inplace=True)
-df = df.tz_localize(tz='utc')
-df.tz_convert('Australia/Brisbane')
-df.reset_index(inplace=True)
+df['startTime'] = df['startTime'] + timedelta(hours=10, minutes=0)
 df['startTime'] = df['startTime'].dt.strftime('%Y-%m-%dT%H:%m:%S')
 
 #Renaming columns and droping unnecessary data
-df.columns = ["StartTime","Region", "MeetingID", "MeetingName", "EventID", "RaceNumber", "RaceLink", "Distance"]
+df.columns = ["Region", "MeetingID", "MeetingName", "EventID", "RaceNumber", "RaceLink", "Distance", "StartTime"]
 result = df[df['Region'] == 'Australia']
 result.drop(['Region'], axis=1, inplace=True)
 
